bb2bce7c0355f163bd9336efff10f3f389cf6184
Justin Nelson <jrnelson90@gmail.com>
2020-07-27 23:47:35 -0600

    Linting cleanup

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ BT "Universal Remote" Control Code for Digital Guitar Amps
 * So far only tested to run on the latest Raspberry Pi OS 32-bit lite image on Pi Zero W and Pi 4B (https://www.raspberrypi.org/downloads/raspberry-pi-os/).
 * `setupLinux.sh` will install necessary Python, I2C, BT, and GPIO libraries.
 * Add `DisablePlugins = pnat` to `/etc/bluetooth/main.conf` post setup install to ensure proper BT pairing.
-
+* Make sure I2C and Bluetooth are enabled, SSH is recommended for headless development
 On client, edit `tinderbox.py` to match your GPIO mapping or OLED screen type before running.
 
 On demo server, edit `demoLedServer.py` to match your LED GPIO mapping before running.
--- src/tinderbox.py
+++ src/tinderbox.py
@@ -1,5 +1,4 @@
 import time
-import traceback
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
@@ -12,13 +11,13 @@ from sys import exit
 # Set Connection Port Default
 server_port = 2
 
-# these are based on the wireshark captures when selecting presets via the app,
+# These are based on Wireshark BT packet captures when selecting presets via the app,
 # the correct commands probably involve reading state, then changing individual bytes before send to amp
 cmdPreset1 = "01fe000053fe1a000000000000000000f00124000138000000f779"
 cmdPreset2 = "01fe000053fe1a000000000000000000f00123010138000001f779"
 cmdPreset3 = "01fe000053fe1a000000000000000000f00125020138000002f779"
 cmdPreset4 = "01fe000053fe1a000000000000000000f00120030138000003f779"
-toneCommands= [cmdPreset1, cmdPreset2, cmdPreset3, cmdPreset4]
+toneCommands = [cmdPreset1, cmdPreset2, cmdPreset3, cmdPreset4]
 
 # Setup Button GPIO
 BUTTON_1 = 20
@@ -34,11 +33,11 @@ GPIO.setup(BUTTON_4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 print("GPIO Setup Complete")
 
 # First define some constants to allow easy resizing of shapes.
-width =128
+width = 128
 height = 64
 padding = -2
 top = padding
-bottom = height-padding
+bottom = height - padding
 
 # Load fonts
 font = ImageFont.load_default()
@@ -52,30 +51,31 @@ serial = i2c(port=1, address=0x3c)
 device = ssd1306(serial, width, height)
 print("I2C OLED Setup Complete")
 
+
 # Define script functions
 def blank_screen():
     print("Blanking OLED Screen")
     with canvas(device) as draw:
-        draw.rectangle((0,0,width,height), outline=0, fill=0)
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-def showStartup():
+
+def show_startup_splash():
     print("Showing Startup Splash")
     with canvas(device) as draw:
         draw.text((0, 20), "TinderBox", font=logo_font, fill=1)
         draw.text((56, 50), "v0.3", fill=1)
     time.sleep(3)
 
-def findBTDevices():
-    found_devices = False
-    while found_devices != True:
+
+def find_bt_devices():
+    while True:
         with canvas(device) as draw:
-            draw.text((20,16), "Scanning For\nBT Devices", font=medium_font, fill=1, align="center")
+            draw.text((20, 16), "Scanning For\nBT Devices", font=medium_font, fill=1, align="center")
         print("Scanning For BT Devices")
         devices = bluetooth.discover_devices(duration=10)
         if devices:
-            response = waitForBTDeviceSelection(devices)
+            response = bt_device_selection(devices)
             if response != "rescan":
-                found_devices = True
                 return response
             else:
                 print("Rescanning for BT Devices")
@@ -83,10 +83,11 @@ def findBTDevices():
             print("No BT Devices Found")
             with canvas(device) as draw:
                 draw.text((0, 20), "Re-scan BT Devices?", font=medium_font, fill=1, align="center")
-            if waitForYNResponse() == False:
+            if not wait_for_yn_response():
                 exit(0)
 
-def waitForBTDeviceSelection(devices):
+
+def bt_device_selection(devices):
     selection_mac = ""
     response = False
     num_of_devices = len(devices)
@@ -94,11 +95,10 @@ def waitForBTDeviceSelection(devices):
     first_loop = True
     menu_top = 0
     menu_bottom = 3
-    menu_size = 4
     print("Found {} BT Devices".format(num_of_devices))
     for d in devices:
         print(d)
-    while response != True:
+    while not response:
         nav_press = False
         if GPIO.input(BUTTON_1) and selected_device != 0:
             print("Device Selection Nav Up Pressed - Button 1")
@@ -111,9 +111,9 @@ def waitForBTDeviceSelection(devices):
             print("Device Selection Nav Down Pressed - Button 3")
             selected_device += 1
             nav_press = True
-            if menu_bottom != num_of_devices-1 and menu_bottom < selected_device:
+            if menu_bottom != num_of_devices - 1 and menu_bottom < selected_device:
                 menu_bottom += 1
-                menu_top +=1
+                menu_top += 1
         if GPIO.input(BUTTON_4):
             print("Device Selection Confirm Pressed - Button 4")
             selection_mac = devices[selected_device]
@@ -122,19 +122,20 @@ def waitForBTDeviceSelection(devices):
             print("Device Selection Rescan Pressed - Button 2")
             selection_mac = "rescan"
             response = True
-        if nav_press == True or first_loop == True:
-            if first_loop == True:
+        if nav_press or first_loop:
+            if first_loop:
                 first_loop = False
-            displayBTDevicesFound(devices, selected_device, menu_top, menu_bottom)
+            display_bt_devices_found(devices, selected_device, menu_top, menu_bottom)
             # Debounce pause
             time.sleep(.1)
     return selection_mac
 
-def displayBTDevicesFound(devices, selected_device, menu_top, menu_bottom):
+
+def display_bt_devices_found(devices, selected_device, menu_top, menu_bottom):
     num_of_devices = len(devices)
     list_space = 12
     with canvas(device) as draw:
-        draw.text((0,0), "  Found {} Devices:".format(num_of_devices), fill=1)
+        draw.text((0, 0), "  Found {} Devices:".format(num_of_devices), fill=1)
         for i, d in enumerate(devices[menu_top:(menu_bottom + 1)], start=menu_top):
             if i == selected_device:
                 draw.text((0, list_space), "->{}".format(d), fill=1)
@@ -142,48 +143,55 @@ def displayBTDevicesFound(devices, selected_device, menu_top, menu_bottom):
                 draw.text((0, list_space), "  {}".format(d), fill=1)
             list_space += 12
 
-def waitForYNResponse():
+
+def wait_for_yn_response():
     response = False
     press = ""
-    while response != True:
+    while not response:
         if GPIO.input(BUTTON_4):
             press = "yes"
         if GPIO.input(BUTTON_3):
             press = "no"
         if press != "":
             response = True
-    return (press == "yes")
+    return press == "yes"
 
-def connectToBTDevice(server_address):
-    print("Connecting to {}".format(server_address))
+
+def connect_to_bt_device(server_addr):
+    print("Connecting to {}".format(server_addr))
     with canvas(device) as draw:
-        draw.text((4,8), "Connecting to\n{}".format(server_address), font=medium_font, fill=1, align="center")
+        draw.text((4, 8), "Connecting to\n{}".format(server_addr), font=medium_font, fill=1, align="center")
     try:
-        client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        client_sock.connect((server_address, server_port))
-        print("Connecting to {} succeeded".format(server_address))
+        client_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        client_socket.connect((server_addr, server_port))
+        print("Connecting to {} succeeded".format(server_addr))
         with canvas(device) as draw:
-            draw.text((4,8), "Connecting to\n{}\nSucceeded".format(server_address), font=medium_font, fill=1, align="center")
+            draw.text((4, 8), "Connecting to\n{}\nSucceeded".format(server_addr), font=medium_font, fill=1,
+                      align="center")
         time.sleep(3)
-        return client_sock
-    except:
-        print("Connecting to {} failed".format(server_address))
+        return client_socket
+    except OSError as e:
+        print(e)
+        print("Connecting to {} failed".format(server_addr))
         with canvas(device) as draw:
-            draw.text((4,8), "Connecting to\n{}\nFailed".format(server_address), font=medium_font, fill=1, align="center")
+            draw.text((4, 8), "Connecting to\n{}\nFailed".format(server_addr), font=medium_font, fill=1,
+                      align="center")
         time.sleep(3)
         return None
 
-def updateSlotOnScreen(selected_slot):
-    with canvas(device) as draw:
-        draw.text((48, top+8), "{}".format(selected_slot), font=large_font, fill=1)
 
-def toneControlLoop(client_sock):
+def update_slot_on_screen(selected_slot):
+    with canvas(device) as draw:
+        draw.text((48, top + 8), "{}".format(selected_slot), font=large_font, fill=1)
+
+
+def tone_control_loop(client_socket):
     with canvas(device) as draw:
         draw.text((24, 16), "Select Initial\nTone Slot", font=medium_font, align="center", fill=1)
     selected_slot = 0
     multi_button_press = 0
     disconnect = False
-    while disconnect != True:
+    while not disconnect:
         new_press = [False, False, False, False]
         if GPIO.input(BUTTON_1):
             new_press[0] = True
@@ -196,17 +204,21 @@ def toneControlLoop(client_sock):
 
         if new_press.count(True) == 1 and new_press.index(True) + 1 != selected_slot:
             selected_slot = new_press.index(True) + 1
-            msg = bytes.fromhex(toneCommands[selected_slot-1])
-            client_sock.send(msg)
-            # Update screen with new selection
-            updateSlotOnScreen(selected_slot)
-            print("Sent \"{}\" to server".format(selected_slot))
-            multi_button_press = 0
-            # Debounce pause
-            time.sleep(.1)
+            msg = bytes.fromhex(toneCommands[selected_slot - 1])
+            try:
+                client_socket.send(msg)
+                # Update screen with new selection
+                update_slot_on_screen(selected_slot)
+                print("Sent \"{}\" to server".format(selected_slot))
+                multi_button_press = 0
+                # Debounce pause
+                time.sleep(.1)
+            except OSError as e:
+                print(e)
+                disconnect = True
         elif new_press.count(True) == 2:
             if multi_button_press >= 5:
-                client_sock.close()
+                client_socket.close()
                 disconnect = True
                 with canvas(device) as draw:
                     draw.text((8, 16), "Disconnected from\nBT Device", font=medium_font, fill=1, align="center")
@@ -218,31 +230,33 @@ def toneControlLoop(client_sock):
         elif new_press.count(True) == 0:
             multi_button_press = 0
 
+
 # Start "main" logic
 def handler(signal_received, frame):
     # Handle any cleanup here
     blank_screen()
     GPIO.cleanup()
     print('SIGINT or CTRL-C detected. Exiting TinderBox.')
-    
-    #TODO: refactor this socket to be passed or global
-    #client_sock.close()
+
+    # TODO: refactor this socket to be passed or global
+    # client_sock.close()
 
     exit(0)
+
 
 if __name__ == '__main__':
     # Tell Python to run the handler() function when SIGINT is recieved
     signal(SIGINT, handler)
 
-showStartup()
+show_startup_splash()
 
 client_sock = None
 try:
     while True:
-        server_address = findBTDevices()
-        client_sock = connectToBTDevice(server_address)
-        if client_sock != None:
-            toneControlLoop(client_sock)
+        server_address = find_bt_devices()
+        client_sock = connect_to_bt_device(server_address)
+        if client_sock is not None:
+            tone_control_loop(client_sock)
 finally:
-    if client_sock != None:
+    if client_sock is not None:
         client_sock.close()
