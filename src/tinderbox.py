67dfb57630be99a27b47195419d3b76686ca95b9
Justin Nelson <jrnelson90@gmail.com>
2020-07-28 08:59:15 -0600

    refactors and code cleanup for tinderbox.py

--- src/tinderbox.py
+++ src/tinderbox.py
@@ -9,15 +9,14 @@ from signal import signal, SIGINT
 from sys import exit
 
 # Set Connection Port Default
-server_port = 2
+SERVER_PORT = 2
 
-# These are based on Wireshark BT packet captures when selecting presets via the app,
-# the correct commands probably involve reading state, then changing individual bytes before send to amp
-cmdPreset1 = "01fe000053fe1a000000000000000000f00124000138000000f779"
-cmdPreset2 = "01fe000053fe1a000000000000000000f00123010138000001f779"
-cmdPreset3 = "01fe000053fe1a000000000000000000f00125020138000002f779"
-cmdPreset4 = "01fe000053fe1a000000000000000000f00120030138000003f779"
-toneCommands = [cmdPreset1, cmdPreset2, cmdPreset3, cmdPreset4]
+# Hex Code Spark Tone Commands
+TONE_1 = "01fe000053fe1a000000000000000000f00124000138000000f779"
+TONE_2 = "01fe000053fe1a000000000000000000f00123010138000001f779"
+TONE_3 = "01fe000053fe1a000000000000000000f00125020138000002f779"
+TONE_4 = "01fe000053fe1a000000000000000000f00120030138000003f779"
+TONE_CMD_LIST = [TONE_1, TONE_2, TONE_3, TONE_4]
 
 # Setup Button GPIO
 BUTTON_1 = 20
@@ -32,13 +31,6 @@ GPIO.setup(BUTTON_3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(BUTTON_4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 print("GPIO Setup Complete")
 
-# First define some constants to allow easy resizing of shapes.
-width = 128
-height = 64
-padding = -2
-top = padding
-bottom = height - padding
-
 # Load fonts
 font = ImageFont.load_default()
 medium_font = ImageFont.truetype('./roboto/Roboto-Regular.ttf', 14)
@@ -47,21 +39,23 @@ large_font = ImageFont.truetype('./Market_Deco.ttf', 56)
 print("Font Setup Complete")
 
 # Setup 128x64 I2C OLED Display:
+SCREEN_WIDTH = 128
+SCREEN_HEIGHT = 64
 serial = i2c(port=1, address=0x3c)
-device = ssd1306(serial, width, height)
+oled_screen = ssd1306(serial, SCREEN_WIDTH, SCREEN_HEIGHT)
 print("I2C OLED Setup Complete")
 
 
 # Define script functions
 def blank_screen():
     print("Blanking OLED Screen")
-    with canvas(device) as draw:
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    with canvas(oled_screen) as draw:
+        draw.rectangle((0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), outline=0, fill=0)
 
 
 def show_startup_splash():
     print("Showing Startup Splash")
-    with canvas(device) as draw:
+    with canvas(oled_screen) as draw:
         draw.text((0, 20), "TinderBox", font=logo_font, fill=1)
         draw.text((56, 50), "v0.3", fill=1)
     time.sleep(3)
@@ -69,7 +63,7 @@ def show_startup_splash():
 
 def find_bt_devices():
     while True:
-        with canvas(device) as draw:
+        with canvas(oled_screen) as draw:
             draw.text((20, 16), "Scanning For\nBT Devices", font=medium_font, fill=1, align="center")
         print("Scanning For BT Devices")
         devices = bluetooth.discover_devices(duration=10)
@@ -81,7 +75,7 @@ def find_bt_devices():
                 print("Rescanning for BT Devices")
         else:
             print("No BT Devices Found")
-            with canvas(device) as draw:
+            with canvas(oled_screen) as draw:
                 draw.text((0, 20), "Re-scan BT Devices?", font=medium_font, fill=1, align="center")
             if not wait_for_yn_response():
                 exit(0)
@@ -134,7 +128,7 @@ def bt_device_selection(devices):
 def display_bt_devices_found(devices, selected_device, menu_top, menu_bottom):
     num_of_devices = len(devices)
     list_space = 12
-    with canvas(device) as draw:
+    with canvas(oled_screen) as draw:
         draw.text((0, 0), "  Found {} Devices:".format(num_of_devices), fill=1)
         for i, d in enumerate(devices[menu_top:(menu_bottom + 1)], start=menu_top):
             if i == selected_device:
@@ -159,13 +153,13 @@ def wait_for_yn_response():
 
 def connect_to_bt_device(server_addr):
     print("Connecting to {}".format(server_addr))
-    with canvas(device) as draw:
+    with canvas(oled_screen) as draw:
         draw.text((4, 8), "Connecting to\n{}".format(server_addr), font=medium_font, fill=1, align="center")
     try:
         client_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        client_socket.connect((server_addr, server_port))
+        client_socket.connect((server_addr, SERVER_PORT))
         print("Connecting to {} succeeded".format(server_addr))
-        with canvas(device) as draw:
+        with canvas(oled_screen) as draw:
             draw.text((4, 8), "Connecting to\n{}\nSucceeded".format(server_addr), font=medium_font, fill=1,
                       align="center")
         time.sleep(3)
@@ -173,7 +167,7 @@ def connect_to_bt_device(server_addr):
     except OSError as e:
         print(e)
         print("Connecting to {} failed".format(server_addr))
-        with canvas(device) as draw:
+        with canvas(oled_screen) as draw:
             draw.text((4, 8), "Connecting to\n{}\nFailed".format(server_addr), font=medium_font, fill=1,
                       align="center")
         time.sleep(3)
@@ -181,12 +175,12 @@ def connect_to_bt_device(server_addr):
 
 
 def update_slot_on_screen(selected_slot):
-    with canvas(device) as draw:
-        draw.text((48, top + 8), "{}".format(selected_slot), font=large_font, fill=1)
+    with canvas(oled_screen) as draw:
+        draw.text((48, 8), "{}".format(selected_slot), font=large_font, fill=1)
 
 
 def tone_control_loop(client_socket):
-    with canvas(device) as draw:
+    with canvas(oled_screen) as draw:
         draw.text((24, 16), "Select Initial\nTone Slot", font=medium_font, align="center", fill=1)
     selected_slot = 0
     multi_button_press = 0
@@ -204,7 +198,7 @@ def tone_control_loop(client_socket):
 
         if new_press.count(True) == 1 and new_press.index(True) + 1 != selected_slot:
             selected_slot = new_press.index(True) + 1
-            msg = bytes.fromhex(toneCommands[selected_slot - 1])
+            msg = bytes.fromhex(TONE_CMD_LIST[selected_slot - 1])
             try:
                 client_socket.send(msg)
                 # Update screen with new selection
@@ -218,15 +212,16 @@ def tone_control_loop(client_socket):
                 disconnect = True
         elif new_press.count(True) == 2:
             if multi_button_press < 6 and multi_button_press <= 3:
-                with canvas(device) as draw:
+                with canvas(oled_screen) as draw:
                     display_secs = 6 - round(multi_button_press, 1)
-                    draw.text((8, 16), "Disconnecting from\nBT Device in {}".format(display_secs), font=medium_font, fill=1, align="center")
+                    draw.text((8, 16), "Disconnecting from\nBT Device in {}".format(display_secs), font=medium_font,
+                              fill=1, align="center")
                 print("Disconnected from server")
                 time.sleep(3)
             if multi_button_press >= 6:
                 client_socket.close()
                 disconnect = True
-                with canvas(device) as draw:
+                with canvas(oled_screen) as draw:
                     draw.text((8, 16), "Disconnected from\nBT Device", font=medium_font, fill=1, align="center")
                 print("Disconnected from server")
                 time.sleep(3)
